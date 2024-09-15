2092603581509acb6a17e5727a09104da0b8613a
chetan791 <chetanchauhan795@gmail.com>
2024-09-16 00:46:34 +0530

    added loader

--- hosteller/src/app/hostels/[HostelsID]/page.tsx
+++ hosteller/src/app/hostels/[HostelsID]/page.tsx
@@ -21,36 +21,7 @@ export default function HostelsID({ params }: { params: any }) {
   const { loading, error, data } = useQuery(gethostels, {
     variables: { locationId: params.HostelsID },
   });
-  console.log(data);
-
-  // console.log(params.HostelsID);
-  // const data = [
-  //   {
-  //     name: "Hostel Five",
-  //     image: "https://example.com/images/hostel5.jpg",
-  //   },
-  //   {
-  //     name: "Hostel Six",
-  //     image: "https://example.com/images/hostel6.jpg",
-  //   },
-  //   {
-  //     name: "Hostel Seven",
-  //     image: "https://example.com/images/hostel7.jpg",
-  //   },
-  //   {
-  //     name: "Hostel Eight",
-  //     image: "https://example.com/images/hostel8.jpg",
-  //   },
-  //   {
-  //     name: "Hostel Nine",
-  //     image: "https://example.com/images/hostel9.jpg",
-  //   },
-  //   {
-  //     name: "Hostel Ten",
-  //     image: "https://example.com/images/hostel10.jpg",
-  //   },
-  // ];
-
+  // console.log(data);
   const addHostelReservation = (payload: any) => {
     alert("Added to cart");
     dispatch(addReservations(payload));
@@ -59,53 +30,63 @@ export default function HostelsID({ params }: { params: any }) {
   return (
     <div>
       <h1 className="text-3xl font-bold text-center p-4">Available Hostels</h1>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4  p-4 w-full">
-        {data?.gethostelbylocation?.map((location: any, index: any) => {
-          return (
-            <div
-              key={index}
-              style={{
-                boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
-              }}
-              className="max-w-sm rounded-lg overflow-hidden shadow-lg w-full border-2"
-            >
-              <Image
-                className="w-full"
-                src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a"
-                alt="Sunset in the mountains"
-                height={200}
-                width={200}
-              />
-              <div className="px-3 py-4">
-                <div className="font-bold text-xl mb-2">{location.name}</div>
-                <p className="text-gray-700 text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. .
-                </p>
-              </div>
-              <div className="flex items-center justify-between px-3 py-4">
-                <div className=" pt-4 pb-2">
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #photography
-                  </span>
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #travel
-                  </span>
-                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                    #winter
-                  </span>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <div className="flex flex-row gap-2">
+            <div className="w-4 h-4 rounded-full bg-[#00b386] animate-bounce"></div>
+            <div className="w-4 h-4 rounded-full bg-[#00b386] animate-bounce [animation-delay:-.3s]"></div>
+            <div className="w-4 h-4 rounded-full bg-[#00b386] animate-bounce [animation-delay:-.5s]"></div>
+          </div>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4  p-4 w-full">
+          {data?.gethostelbylocation?.map((location: any, index: any) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                }}
+                className="max-w-sm rounded-lg overflow-hidden shadow-lg w-full border-2"
+              >
+                <Image
+                  className="w-full"
+                  src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a"
+                  alt="Sunset in the mountains"
+                  height={200}
+                  width={200}
+                />
+                <div className="px-3 py-4">
+                  <div className="font-bold text-xl mb-2">{location.name}</div>
+                  <p className="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. .
+                  </p>
                 </div>
-                <button
-                  style={{ backgroundColor: "#00b386" }}
-                  className=" text-white font-bold py-2 px-4 rounded-full"
-                  onClick={() => addHostelReservation(location)}
-                >
-                  Make Bookings
-                </button>
+                <div className="flex items-center justify-between px-3 py-4">
+                  <div className=" pt-4 pb-2">
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      #photography
+                    </span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      #travel
+                    </span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      #winter
+                    </span>
+                  </div>
+                  <button
+                    style={{ backgroundColor: "#00b386" }}
+                    className=" text-white font-bold py-2 px-4 rounded-full"
+                    onClick={() => addHostelReservation(location)}
+                  >
+                    Make Bookings
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
